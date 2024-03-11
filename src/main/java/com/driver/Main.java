239904884260d72b5aac4caada2d406646e797ab
kunalahire1 <kunalahire9607@gmail.com>
2024-03-11 18:33:50 +0530

    done21

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,5 +2,10 @@ package com.driver;
 
 public class Main {
     //hello
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+        obj.setName("kunal");
+        System.out.println(obj.getName());
+    }
   
 }
