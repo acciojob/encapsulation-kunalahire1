5b202894dec1b5065baf20fa32961353264065bb
Kunal Ahire <123294963+kunalahire1@users.noreply.github.com>
2024-03-11 17:55:16 +0530

    Main.java

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -3,6 +3,10 @@ package com.driver.test;
 import static org.junit.Assert.*;
 import org.junit.jupiter.api.Test;
 
-public class TestCases {
-    
+public class Main {
+    public static void main(String[] args) {
+        RWOnly obj=new RWOnly();
+        obj.setName("hello kunal");
+        System.out.println(obj.getName());
+    }
 }
